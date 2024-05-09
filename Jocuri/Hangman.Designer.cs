8802a8677f3c50a4081ca9ff485c0e3908c67174
Ruxandra Horhat <r.m.horhat@hotmail.com>
2024-05-09 21:28:45 +0300

    update4

--- Jocuri/Hangman.Designer.cs
+++ Jocuri/Hangman.Designer.cs
@@ -62,7 +62,7 @@ namespace Jocuri
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(459, 433);
+            this.label2.Location = new System.Drawing.Point(530, 234);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 34);
             this.label2.TabIndex = 1;
@@ -75,7 +75,7 @@ namespace Jocuri
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(489, 433);
+            this.label3.Location = new System.Drawing.Point(560, 234);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(30, 34);
             this.label3.TabIndex = 2;
@@ -88,7 +88,7 @@ namespace Jocuri
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(533, 433);
+            this.label4.Location = new System.Drawing.Point(604, 234);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(30, 34);
             this.label4.TabIndex = 3;
@@ -101,7 +101,7 @@ namespace Jocuri
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(572, 433);
+            this.label5.Location = new System.Drawing.Point(643, 234);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(30, 34);
             this.label5.TabIndex = 4;
@@ -114,7 +114,7 @@ namespace Jocuri
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(617, 433);
+            this.label6.Location = new System.Drawing.Point(688, 234);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(30, 34);
             this.label6.TabIndex = 5;
@@ -127,7 +127,7 @@ namespace Jocuri
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(660, 433);
+            this.label7.Location = new System.Drawing.Point(731, 234);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(30, 34);
             this.label7.TabIndex = 6;
@@ -140,7 +140,7 @@ namespace Jocuri
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(706, 433);
+            this.label8.Location = new System.Drawing.Point(777, 234);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(30, 34);
             this.label8.TabIndex = 7;
@@ -153,7 +153,7 @@ namespace Jocuri
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(749, 433);
+            this.label9.Location = new System.Drawing.Point(820, 234);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(30, 34);
             this.label9.TabIndex = 8;
@@ -167,7 +167,7 @@ namespace Jocuri
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(789, 433);
+            this.label10.Location = new System.Drawing.Point(860, 234);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(30, 34);
             this.label10.TabIndex = 9;
@@ -180,7 +180,7 @@ namespace Jocuri
             this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(819, 433);
+            this.label11.Location = new System.Drawing.Point(890, 234);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(30, 34);
             this.label11.TabIndex = 10;
@@ -193,7 +193,7 @@ namespace Jocuri
             this.label12.BackColor = System.Drawing.Color.Transparent;
             this.label12.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.Color.White;
-            this.label12.Location = new System.Drawing.Point(862, 433);
+            this.label12.Location = new System.Drawing.Point(933, 234);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(30, 34);
             this.label12.TabIndex = 11;
@@ -220,7 +220,7 @@ namespace Jocuri
             this.label15.BackColor = System.Drawing.Color.Transparent;
             this.label15.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label15.ForeColor = System.Drawing.Color.White;
-            this.label15.Location = new System.Drawing.Point(446, 79);
+            this.label15.Location = new System.Drawing.Point(530, 83);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(208, 32);
             this.label15.TabIndex = 15;
@@ -233,7 +233,7 @@ namespace Jocuri
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(862, 419);
+            this.label1.Location = new System.Drawing.Point(933, 220);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 34);
             this.label1.TabIndex = 26;
@@ -247,7 +247,7 @@ namespace Jocuri
             this.label16.BackColor = System.Drawing.Color.Transparent;
             this.label16.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label16.ForeColor = System.Drawing.Color.White;
-            this.label16.Location = new System.Drawing.Point(819, 419);
+            this.label16.Location = new System.Drawing.Point(890, 220);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(37, 34);
             this.label16.TabIndex = 25;
@@ -261,7 +261,7 @@ namespace Jocuri
             this.label17.BackColor = System.Drawing.Color.Transparent;
             this.label17.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label17.ForeColor = System.Drawing.Color.White;
-            this.label17.Location = new System.Drawing.Point(789, 419);
+            this.label17.Location = new System.Drawing.Point(860, 220);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(24, 34);
             this.label17.TabIndex = 24;
@@ -275,7 +275,7 @@ namespace Jocuri
             this.label18.BackColor = System.Drawing.Color.Transparent;
             this.label18.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label18.ForeColor = System.Drawing.Color.White;
-            this.label18.Location = new System.Drawing.Point(749, 419);
+            this.label18.Location = new System.Drawing.Point(820, 220);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(34, 34);
             this.label18.TabIndex = 23;
@@ -289,7 +289,7 @@ namespace Jocuri
             this.label19.BackColor = System.Drawing.Color.Transparent;
             this.label19.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label19.ForeColor = System.Drawing.Color.White;
-            this.label19.Location = new System.Drawing.Point(706, 419);
+            this.label19.Location = new System.Drawing.Point(777, 220);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(37, 34);
             this.label19.TabIndex = 22;
@@ -303,7 +303,7 @@ namespace Jocuri
             this.label20.BackColor = System.Drawing.Color.Transparent;
             this.label20.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label20.ForeColor = System.Drawing.Color.White;
-            this.label20.Location = new System.Drawing.Point(660, 419);
+            this.label20.Location = new System.Drawing.Point(731, 220);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(40, 34);
             this.label20.TabIndex = 21;
@@ -317,7 +317,7 @@ namespace Jocuri
             this.label21.BackColor = System.Drawing.Color.Transparent;
             this.label21.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label21.ForeColor = System.Drawing.Color.White;
-            this.label21.Location = new System.Drawing.Point(617, 419);
+            this.label21.Location = new System.Drawing.Point(688, 220);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(37, 34);
             this.label21.TabIndex = 20;
@@ -331,7 +331,7 @@ namespace Jocuri
             this.label22.BackColor = System.Drawing.Color.Transparent;
             this.label22.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label22.ForeColor = System.Drawing.Color.White;
-            this.label22.Location = new System.Drawing.Point(572, 419);
+            this.label22.Location = new System.Drawing.Point(643, 220);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(39, 34);
             this.label22.TabIndex = 19;
@@ -345,7 +345,7 @@ namespace Jocuri
             this.label23.BackColor = System.Drawing.Color.Transparent;
             this.label23.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label23.ForeColor = System.Drawing.Color.White;
-            this.label23.Location = new System.Drawing.Point(533, 419);
+            this.label23.Location = new System.Drawing.Point(604, 220);
             this.label23.Name = "label23";
             this.label23.Size = new System.Drawing.Size(33, 34);
             this.label23.TabIndex = 18;
@@ -359,7 +359,7 @@ namespace Jocuri
             this.label24.BackColor = System.Drawing.Color.Transparent;
             this.label24.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label24.ForeColor = System.Drawing.Color.White;
-            this.label24.Location = new System.Drawing.Point(489, 419);
+            this.label24.Location = new System.Drawing.Point(560, 220);
             this.label24.Name = "label24";
             this.label24.Size = new System.Drawing.Size(38, 34);
             this.label24.TabIndex = 17;
@@ -373,7 +373,7 @@ namespace Jocuri
             this.label25.BackColor = System.Drawing.Color.Transparent;
             this.label25.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label25.ForeColor = System.Drawing.Color.White;
-            this.label25.Location = new System.Drawing.Point(459, 419);
+            this.label25.Location = new System.Drawing.Point(530, 220);
             this.label25.Name = "label25";
             this.label25.Size = new System.Drawing.Size(24, 34);
             this.label25.TabIndex = 16;
@@ -413,7 +413,7 @@ namespace Jocuri
             this.Controls.Add(this.label2);
             this.Name = "Hangman";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Hangman";
+            this.Text = " ";
             this.Load += new System.EventHandler(this.Hangman_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Hangman_KeyDown);
             this.ResumeLayout(false);
--- Jocuri/Hangman.cs
+++ Jocuri/Hangman.cs
@@ -12,7 +12,8 @@ namespace Jocuri
 {
     public partial class Hangman : Form
     {
-        string word = "INFORMATICA";
+        string usedLetters = "";
+        int mistake = 0, correct = 0;
 
         public Hangman()
         {
@@ -20,34 +21,134 @@ namespace Jocuri
             label15.Text = "Litere greșite: ";
         }
 
+        void reset()
+        {
+            label1.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            label22.Visible = false;
+            label23.Visible = false;
+            label24.Visible = false;
+            label25.Visible = false;
+            label15.Text = "Litere greșite: ";
+            mistake = 0;
+            correct = 0;
+            usedLetters = "";
+            Refresh();
+        }
+
+
+        void draw()
+        {
+            Graphics e;
+            e = this.CreateGraphics();
+            if (mistake == 1)
+            {
+                e.DrawLine(new Pen(Color.White, 3), 50, 375, 300, 375);
+                e.DrawLine(new Pen(Color.White, 3), 175, 375, 175, 100);
+                e.DrawLine(new Pen(Color.White, 3), 175, 100, 300, 100);
+                e.DrawLine(new Pen(Color.White, 3), 300, 100, 300, 150);
+            }
+            else if (mistake == 2)
+                e.DrawEllipse(new Pen(Color.White, 3), 275, 150, 50, 50);
+            else if (mistake == 3)
+                e.DrawLine(new Pen(Color.White, 3), 300, 200, 300, 275);
+            else if (mistake == 4)
+                e.DrawLine(new Pen(Color.White, 3), 300, 210, 250, 240);
+            else if (mistake == 5)
+                e.DrawLine(new Pen(Color.White, 3), 300, 210, 350, 240);
+            else if (mistake == 6)
+                e.DrawLine(new Pen(Color.White, 3), 300, 275, 250, 305);
+            else if (mistake == 7)
+            {
+                e.DrawLine(new Pen(Color.White, 3), 300, 275, 350, 305);
+                MessageBox.Show("Ai pierdut!");
+                reset();
+            }
+        }
+
         void verify(KeyEventArgs letter)
         {
-            if (letter.KeyCode == Keys.I)
+            if(65 <= (char)letter.KeyCode && (char)letter.KeyCode <= 97)
             {
-                label25.Visible = true;
-                label17.Visible = true;
+                if (usedLetters.IndexOf((char)letter.KeyCode) != -1)
+                    MessageBox.Show("Ai folosit deja litera!");
+                else
+                {
+                    if (letter.KeyCode == Keys.I)
+                    {
+                        label25.Visible = true;
+                        label17.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.N)
+                    {
+                        label24.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.F)
+                    {
+                        label23.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.O)
+                    {
+                        label22.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.R)
+                    {
+                        label21.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.M)
+                    {
+                        label20.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.A)
+                    {
+                        label19.Visible = true;
+                        label1.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.T)
+                    {
+                        label18.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else if (letter.KeyCode == Keys.C)
+                    {
+                        label16.Visible = true;
+                        correct++;
+                        usedLetters += letter.KeyCode.ToString();
+                    }
+                    else
+                    {
+                        label15.Text += letter.KeyCode.ToString() + " ";
+                        mistake++;
+                        usedLetters += letter.KeyCode.ToString();
+                        draw();
+                    }
+                }
+                if (correct == 9)
+                {
+                    MessageBox.Show("Ai câștigat!");
+                    reset();
+                }
             }
-            else if (letter.KeyCode == Keys.N)
-                label24.Visible = true;
-            else if (letter.KeyCode == Keys.F)
-                label23.Visible = true;
-            else if (letter.KeyCode == Keys.O)
-                label22.Visible = true;
-            else if (letter.KeyCode == Keys.R)
-                label21.Visible = true;
-            else if (letter.KeyCode == Keys.M)
-                label20.Visible = true;
-            else if (letter.KeyCode == Keys.A)
-            {
-                label19.Visible = true;
-                label1.Visible = true;
-            }
-            else if (letter.KeyCode == Keys.T)
-                label18.Visible = true;
-            else if (letter.KeyCode == Keys.C)
-                label16.Visible = true;
-            else
-                label15.Text += letter.KeyCode.ToString() + " ";
         }
 
         private void Hangman_Load(object sender, EventArgs e)
@@ -78,7 +179,6 @@ namespace Jocuri
         private void Hangman_KeyDown(object sender, KeyEventArgs e)
         {
             verify(e);
-                
         }
     }
 }
